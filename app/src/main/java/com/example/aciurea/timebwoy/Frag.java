4a571762a13b3677cccbe18fbc756966a492d0fe
Andrei Ciurea <andrei.ciurea@endava.com>
2017-11-01 17:26:42 +0200

    frag

---
+++ app/src/main/java/com/example/aciurea/timebwoy/Frag.java
@@ -0,0 +1,20 @@
+package com.example.aciurea.timebwoy;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aciurea on 11/1/2017.
+ */
+
+public class Frag extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.frag_view, container, false);
+    }
+}
